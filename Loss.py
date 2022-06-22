b803b7a3871cc38a771362980a9c386761030ecf
hengding.wang <hengding.wang@gmail.com>
2022-06-22 12:14:59 +0800

    待调试

---
+++ Loss.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+
+
+class ContrastiveLoss(nn.Module):
+
+    def __init__(self, temperature=0.07, base_temperature=0.07):
+        super(ContrastiveLoss, self).__init__()
+        self.temperature = temperature
+        self.base_temperature = base_temperature
+
+    def forward(self, features, labels):
+        device = (torch.device('cuda') if features.is_cuda else torch.device('cpu'))
+
+        batch_size = features.shape[0]
+        labels = labels.contiguous().view(-1, 1)
+        mask = torch.eq(labels, labels.T).float().to(device)
+
+        # compute logits
+        anchor_dot_contrast = torch.div(torch.matmul(features, features.T), self.temperature)
+
+        # for numerical stability
+        logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
+        logits = anchor_dot_contrast - logits_max.detach()  # todo why
+
+        # mask-out self-contrast cases
+        logits_mask = torch.scatter(torch.ones_like(mask), 1, torch.arange(batch_size).view(-1, 1).to(device), 0)
+        mask = mask * logits_mask
+
+        # compute log_prob
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+
+        # compute mean of log-likelihood over positive
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+
+        # loss
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        return loss.mean()
---
+++ Network.py
@@ -0,0 +1,49 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class ConNetwork(nn.Module):
+    """Linear classifier"""
+
+    def __init__(self, num_classes):
+        super(ConNetwork, self).__init__()
+        self.num_classes = num_classes
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 16, kernel_size=3), nn.ReLU(), nn.MaxPool2d(2))
+        self.conv2 = nn.Sequential(nn.Conv2d(16, 32, kernel_size=3), nn.ReLU(), nn.MaxPool2d(2))
+
+        self.projs = []
+
+        for i in range(num_classes):
+            cls = nn.Sequential(nn.Linear(32 * 6 * 6, 128), nn.ReLU(), nn.Linear(128, 64))
+            self.projs.append(cls)
+
+    def encoder(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(-1, 32 * 6 * 6)
+        return x
+
+    def forward(self, x):
+        x = self.encoder(x)
+        out = []
+        for i in range(self.num_classes):
+            out.append(F.normalize(self.projs[i](x), dim=1))
+        return out
+
+
+class LinearClassifier(nn.Module):
+    """Linear classifier"""
+
+    def __init__(self, num_classes=10):
+        super(LinearClassifier, self).__init__()
+
+        self.classifiers = []
+        for i in range(num_classes):
+            cls = nn.Sequential(nn.Linear(32 * 6 * 6, 128), nn.ReLU(), nn.Linear(128, 1), nn.Sigmoid())
+            self.classifiers.append(cls)
+
+    def forward(self, x):
+        out = []
+        for i in range(self.num_classes):
+            out.append(self.classifiers[i](x), dim=1)
+        return out
